641ebcd4c362368c869d59a327fe3b8e18da95c5
naakheel <naakheel@indiana.edu>
2012-04-26 23:25:13 -0400

    Added comments and exception handling

--- core/org.cishell.workflow/src/org/cishell/workflow/Workflow.java
+++ core/org.cishell.workflow/src/org/cishell/workflow/Workflow.java
@@ -1,20 +1,35 @@
 package org.cishell.workflow;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 
+/**
+ * The Class Workflow.
+ * 
+ * Base workflow instance singleton that stores the current workflow state
+ * and the recorded algorithm list.
+ * 
+ * Author: P632
+ */
 public class Workflow {
 
 	private static Workflow instance = null;
 	private boolean isRecord;
 	private ArrayList<String> algorithmList;
 	
+	/**
+	 * Instantiates a new workflow.
+	 */
 	protected Workflow() {
 
 		isRecord = false;
 		algorithmList = new ArrayList<String>();
 	}
 
+	/**
+	 * Gets the single instance of Workflow.
+	 *
+	 * @return single instance of Workflow
+	 */
 	public static Workflow getInstance() {
 		if (instance == null) {
 			instance = new Workflow();
@@ -22,26 +37,54 @@ public class Workflow {
 		return instance;
 	}
 
+	/**
+	 * Checks if is record.
+	 *
+	 * @return true, if is record
+	 */
 	public boolean isRecord() {
 		return isRecord;
 	}
 
+	/**
+	 * Sets the record.
+	 *
+	 * @param isRecord the new record
+	 */
 	public void setRecord(boolean isRecord) {
 		this.isRecord = isRecord;
 	}
 
+	/**
+	 * Gets the algorithm list.
+	 *
+	 * @return the algorithm list
+	 */
 	public ArrayList<String> getAlgorithmList() {
 		return algorithmList;
 	}
 
+	/**
+	 * Sets the algorithm list.
+	 *
+	 * @param algorithmList the new algorithm list
+	 */
 	public void setAlgorithmList(ArrayList<String> algorithmList) {
 		this.algorithmList = algorithmList;
 	}
 	
+	/**
+	 * Adds the to algorithm list.
+	 *
+	 * @param value the value
+	 */
 	public void addToAlgorithmList(String value){
 		this.algorithmList.add(value);
 	}
 	
+	/**
+	 * Empty algorithm list.
+	 */
 	public void emptyAlgorithmList(){
 		this.algorithmList.clear();
 	}
--- core/org.cishell.workflow/src/org/cishell/workflow/WorkflowListener.java
+++ core/org.cishell.workflow/src/org/cishell/workflow/WorkflowListener.java
@@ -10,36 +10,69 @@ import org.osgi.framework.ServiceReference;
 
 /**
  * Listens for notification from the scheduler and notifies all registered
- * objects
+ * objects.
+ * The listener interface for receiving workflow events.
+ * The class that is interested in processing a workflow
+ * event implements this interface, and the object created
+ * with that class is registered with a component using the
+ * component's <code>addWorkflowListener<code> method. When
+ * the workflow event occurs, that object's appropriate
+ * method is invoked.
+ *
+ * Author: P632
  */
 public class WorkflowListener implements SchedulerListener {
 
+	/** The Constant INSTANCE. */
 	private static final WorkflowListener INSTANCE = new WorkflowListener();
 
+	/**
+	 * Instantiates a new workflow listener.
+	 */
 	private WorkflowListener() {
 		SchedulerContentModel.getInstance().register(this);
 	}
 
+	/**
+	 * Gets the single instance of WorkflowListener.
+	 *
+	 * @return single instance of WorkflowListener
+	 */
 	public static WorkflowListener getInstance() {
 		return INSTANCE;
 	}
 
+	/* (non-Javadoc)
+	 * @see org.cishell.app.service.scheduler.SchedulerListener#algorithmScheduled(org.cishell.framework.algorithm.Algorithm, java.util.Calendar)
+	 */
 	public void algorithmScheduled(Algorithm algorithm, Calendar time) {
 
 	}
 
+	/* (non-Javadoc)
+	 * @see org.cishell.app.service.scheduler.SchedulerListener#algorithmRescheduled(org.cishell.framework.algorithm.Algorithm, java.util.Calendar)
+	 */
 	public void algorithmRescheduled(Algorithm algorithm, Calendar time) {
 
 	}
 
+	/* (non-Javadoc)
+	 * @see org.cishell.app.service.scheduler.SchedulerListener#algorithmUnscheduled(org.cishell.framework.algorithm.Algorithm)
+	 */
 	public void algorithmUnscheduled(Algorithm algorithm) {
 
 	}
 
+	/* (non-Javadoc)
+	 * @see org.cishell.app.service.scheduler.SchedulerListener#algorithmStarted(org.cishell.framework.algorithm.Algorithm)
+	 */
 	public void algorithmStarted(Algorithm algorithm) {
 
 	}
 
+	/* (non-Javadoc)
+	 * @see org.cishell.app.service.scheduler.SchedulerListener#algorithmFinished(org.cishell.framework.algorithm.Algorithm, org.cishell.framework.data.Data[])
+	 */
 	public void algorithmFinished(Algorithm algorithm, Data[] createdData) {
 		Workflow workFlow = Workflow.getInstance();
 		if (workFlow.isRecord()) {
@@ -49,21 +82,31 @@ public class WorkflowListener implements SchedulerListener {
 			if (serviceReference != null) {
 				servicePID = (String) serviceReference
 						.getProperty("service.pid");
-				if(!servicePID.contains("StartRecord") && !servicePID.contains("StopRecord")){
+				if (!servicePID.contains("StartRecord")
+						&& !servicePID.contains("StopRecord")) {
 					workFlow.addToAlgorithmList(servicePID);
 				}
 			}
 		}
 	}
 
+	/* (non-Javadoc)
+	 * @see org.cishell.app.service.scheduler.SchedulerListener#algorithmError(org.cishell.framework.algorithm.Algorithm, java.lang.Throwable)
+	 */
 	public void algorithmError(Algorithm algorithm, Throwable error) {
 
 	}
 
+	/* (non-Javadoc)
+	 * @see org.cishell.app.service.scheduler.SchedulerListener#schedulerRunStateChanged(boolean)
+	 */
 	public void schedulerRunStateChanged(boolean isRunning) {
 
 	}
 
+	/* (non-Javadoc)
+	 * @see org.cishell.app.service.scheduler.SchedulerListener#schedulerCleared()
+	 */
 	public void schedulerCleared() {
 
 	}
